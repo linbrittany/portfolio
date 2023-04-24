cc8ca1b92f107b46f5972923bf758ed7bdf4e2b3
linbrittany <brlin@itba.edu.ar>
2023-04-23 22:15:00 -0300

    fixes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React from 'react';
 import { Link } from "react-router-dom";
-import { FaLinkedin, FaTwitter} from "react-icons/fa"
+import { FaLinkedin} from "react-icons/fa"
 import {BiEnvelope} from "react-icons/bi"
 import {BsGithub} from "react-icons/bs"
 import "../styles/Contact.css";
+import { MdLocationOn, MdOutlineAlternateEmail, MdPhone } from "react-icons/md";
 import { motion } from "framer-motion";
 
 const Contact = () => {
@@ -28,75 +29,29 @@ const Contact = () => {
         <div className="contact" id='contact'>
             <div className="container">
                 <motion.div className="heading" initial={{opacity: 0}} whileInView={fade} viewport={{ once: true }}>
-                    <p className='heading-text'>Say Hello</p>
+                    <p className='heading-text'>Contact</p>
                 </motion.div>
                 <div className="contact-box">
-                    <motion.div className="left-box" initial={{opacity: 0, y: '-50px'}} whileInView={verticalLeft}>
-                        <div className="contact-heading">
-                            <p>If you have any question or request, please contact me!</p>
+                    <div className='contact-list'>
+                        <div className="contact-item">
+                            <div className='item-icon'><MdOutlineAlternateEmail /></div>
+                            <p className='item-text'>brittanylin00@gmail.com</p>
                         </div>
-                        <div className="contact-hello">
-                            <p>Contacts</p>
-                            <ul className='nav-links'>
-                                <li><Link  to="//www.linkedin.com/in/linbrittany" target='_blank' className='contact-icon'><FaLinkedin /></Link></li>
-                                <li><Link  to="//github.com/linbrittany" target='_blank' className='contact-icon'><BsGithub /></Link></li>
-                                <li><Link  to="//mailto:brittanylin00@gmail.com" target='_blank' className='contact-icon'><BiEnvelope /></Link></li>
-                            </ul>
+                        <div className="contact-item">
+                            <div className='item-icon'><MdPhone /></div>
+                            <p className='item-text'>(+54) 9 11-3022-0578</p>
                         </div>
-                    </motion.div>
-                    <motion.div className="right-box" initial={{opacity: 0, y: '50px'}} whileInView={verticalLeft}>
-                    
-                        <form
-                            name="contact-form"
-                            method="POST"
-                            data-netlify="true"
-                            action="POST"
-                        >
-                            <input type="hidden" name="form-name" value="contact-form" />
-                            <div className="form-top">
-                            <div className="name">
-                                <label htmlFor="name">Your Name</label>
-                                <input
-                                type="text"
-                                name="name"
-                                id="name"
-                                placeholder="Enter your name"
-                                required
-                                />
-                            </div>
-
-                            <div className="email">
-                                <label htmlFor="email">Your Email</label>
-                                <input
-                                type="email"
-                                name="email"
-                                id="email"
-                                placeholder="Enter your email address"
-                                required
-                                />
-                            </div>
-                            </div>
-
-                            <div className="form-mid">
-                            <div className="message">
-                                <label htmlFor="message">Your message</label>
-                                <textarea
-                                type="text"
-                                name="message"
-                                id="message"
-                                placeholder="Hi, I would like to contact you!"
-                                required
-                                ></textarea>
-                            </div>
-                            </div>
-
-                            <div className="form-btn">
-                            <button type="submit" className="hero-contact">
-                                Send Message
-                            </button>
-                            </div>
-                        </form>
-                    </motion.div>
+                        <div className="contact-item">
+                            <div className='item-icon'><MdLocationOn /></div>
+                            <p className='item-text'>Buenos Aires, Argentina</p>
+                        </div>
+                    </div>
+                    <div className="contact-hello">
+                        <ul className='nav-links'>
+                            <li><Link  to="//www.linkedin.com/in/linbrittany" target='_blank' className='contact-icon'><FaLinkedin /></Link></li>
+                            <li><Link  to="//github.com/linbrittany" target='_blank' className='contact-icon'><BsGithub /></Link></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>

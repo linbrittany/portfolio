cc2f3fe82e948a40058d1847fc24e2b6d72c6972
linbrittany <brlin@itba.edu.ar>
2023-04-24 10:18:33 -0300

    fix: profile img

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import "../styles/About.css"
 import { motion } from "framer-motion";
-import ProfileImg from '../images/profile.png'
+import ProfileImg from '../images/about.png'
 
 const About = () => {  
   
